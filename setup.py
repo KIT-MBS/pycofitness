407823672f30ac9bbd0043bd9018b2395589c7f4
Computational Biology and Bioinformatics <62349016+3BioCompBio@users.noreply.github.com>
2024-01-22 11:13:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ plmdca_ext = Extension(
 
 setup(
     name="pycofitness",
-    version="1.1",
+    version="1.3",
     author="Fabrizio Pucci, Mehari B. Zerihun",
     author_email="Fabrizio.Pucci@ulb.be, mbzerihun@gmail.com",
     python_requires=">=3.6",
